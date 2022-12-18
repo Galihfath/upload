b5a323055d03805d915d6c3960fe33b9af1925a0
galih fathurrochman <112954226+Galihfath@users.noreply.github.com>
2022-12-18 23:55:26 +0700

    upload

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "finalproject-galleryapp-tania.netlify.app" // TODO: replace this
+const studentName = "Galih Fathurrochman" // TODO: replace this
+const studentId = "FE3094106" // TODO: replace this
+
+export { url, studentName, studentId }
